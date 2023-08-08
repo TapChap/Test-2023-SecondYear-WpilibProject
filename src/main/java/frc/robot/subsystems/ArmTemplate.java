2ea8255e5f68d0bbabdfd3347b5c71b073075a1e
Shai Grossman <72968565+TapChap@users.noreply.github.com>
2023-08-08 17:19:58 +0300

    fixed template for makeup test

--- src/main/java/frc/robot/subsystems/ArmTemplate.java
+++ src/main/java/frc/robot/subsystems/ArmTemplate.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Arm extends SubsystemBase {
+public class ArmTemplate extends SubsystemBase {
 
   public void toggleGamePieceSetting() {}
 
